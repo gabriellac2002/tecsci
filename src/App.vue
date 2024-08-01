b7c1ab9b7e94a4f33f23720760a8ac28e499988f
Gabriella <gabriellacsilva2002@gmail.com>
2024-07-31 22:44:35 -0300

    implementando navbar e footer

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,47 @@
 <template>
-  <h1>teste</h1>
+  <div>
+    <Navbar :logo="logo_src" :alt="app_name" />
+    <div>
+      <router-view></router-view>
+    </div>
+    <Footer />
+  </div>
 </template>
-
 <script>
+import Navbar from './components/Navbar.vue';
+import Footer from './components/Footer.vue';
 
 export default {
-  name: 'App',
+  data: function() {
+    return {
+      logo_src: "/assets/logo.png",
+      app_name: "Logo TECSCI"
+    }
+  },
+  components: {
+    Navbar,
+    Footer
+  }
 }
 </script>
+
+<style>
+  * {
+    font-family: Helvetica;
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+  }
+
+  .main-container {
+    margin: 50px;
+    min-height: 250px;
+  }
+
+  h1 {
+    text-align: center;
+    font-size: 42px;
+    margin-bottom: 30px;
+    color: #222;
+  }
+</style>
