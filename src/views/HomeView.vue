472f797df220ec664e67a11f3a809775de156134
Gabriella <gabriellacsilva2002@gmail.com>
2024-07-31 22:15:56 -0300

    adicionando jsn-server e data

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,12 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <h1>Home</h1>
   </main>
 </template>
+
+<style>
+</style>
